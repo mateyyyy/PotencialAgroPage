b26df47f15bf3cc65e7cc9d97fab83d4e9e681df
Pablo Gimenez <matiixd11@gmail.com>
2024-09-16 16:17:26 -0300

    NuevoLogo

--- src/components/TercerApartado/index.jsx
+++ src/components/TercerApartado/index.jsx
@@ -5,7 +5,9 @@ export default function TercerApartado() {
   return (
     <div id={styles.PrincipalContainer}>
         <h1 id={styles.quienesSomos}>¿QUIENES SOMOS?</h1>
-        
+        <div className={styles.TemplateContainer}>
+          <img src="G:\UNViMe\IngSoftwareII\PaginaPotencialAgro\PotencialAgro\src\assets\MenuPrincipalNoBackground.png" alt="" />
+        </div>
     </div>
   )
 }
