27998761df1ef7469a86174c03597f5e2af1e52f
Pablo Gimenez <matiixd11@gmail.com>
2024-09-17 18:17:04 -0300

    Responsive document section fixed

--- src/components/ProyectoApartado/index.jsx
+++ src/components/ProyectoApartado/index.jsx
@@ -13,14 +13,14 @@ export default function ProyectoApartado() {
       <h1 id={styles.titulo}>MANUALES Y DOCUMENTOS</h1>
         <div id={styles.PrinDiv}>
           <div className={styles.Card}>
-            <a href="">
+            <a href="" className={styles.link}>
               <div className={styles.cardInfo}>
                 <RiOrganizationChart style={{ width: '5vw', height: 'auto' }}/>
                 <h3 className={styles.cardInfoH3}>ORGANIGRAMA</h3>
               </div>
             </a>
 
-            <Link to={"/alcance"}>
+            <Link to={"/alcance"} className={styles.link}>
               <div className={styles.cardInfo}>
                 <GiArrowScope style={{ width: '5vw', height: 'auto' }}/>
                 <h3 className={styles.cardInfoH3}>ALCANCE DEL PROYECTO</h3>
@@ -30,14 +30,14 @@ export default function ProyectoApartado() {
           </div>
 
           <div className={styles.Card}>
-            <Link to={"/acta"}>
+            <Link to={"/acta"} className={styles.link}>
               <div className={styles.cardInfo}>
                 <GrDocumentUser style={{ width: '5vw', height: 'auto' }}/>
                 <h3 className={styles.cardInfoH3}>ACTA DE CONSTITUCION</h3>
               </div>
             </Link>
 
-            <Link to={"/requisitos"}>
+            <Link to={"/requisitos"} className={styles.link}>
               <div className={styles.cardInfo}>
                 <HiClipboardDocumentCheck  style={{ width: '5vw', height: 'auto' }}/>
                 <h3 className={styles.cardInfoH3}>DOCUMENTO DE REQUISITOS</h3>
