0aebbf09025d9ea62f860ef11c867ee869c2b804
Pablo Gimenez <matiixd11@gmail.com>
2024-09-17 15:55:37 -0300

    Bugs responsive fixeados

--- src/components/SegundoApartado/index.jsx
+++ src/components/SegundoApartado/index.jsx
@@ -10,20 +10,20 @@ export default function SegundoApartado() {
   return (
     <div id={styles.PrincipalContainer}>
       <div className={styles.cardContainer}>
-        <MdOutlineSensors style={{ width: '5vw', height: 'auto' }}/> 
+        <MdOutlineSensors className={styles.icon}/> 
         <h2 className={styles.h2Title}>Seguimiento de cultivos mediante sensores</h2>
         <p>El sistema recolecta y mostrará los datos de humedad, pH, temperatura, cantidad de luz y nutrientes del suelo, obtenidos a través de los sensores instalados en el campo. </p>
       </div>
 
       <div className={styles.cardContainer}>   
-        <MdOutlineLineAxis style={{ width: '5vw', height: 'auto' }}/>
+        <MdOutlineLineAxis className={styles.icon}/>
         <h2 className={styles.h2Title}>Visualización gráfica del análisis de los datos</h2>
         <p>Los datos recolectados por los sensores se mostrarán en forma de gráficos que permitirán al usuario interactuar con ellos. 
         </p>
       </div>
 
       <div className={styles.cardContainer}>
-        <BsRobot style={{ width: '5vw', height: 'auto' }}/>
+        <BsRobot className={styles.icon}/>
         <h2 className={styles.h2Title}>Análisis de datos con Inteligencia Artificial</h2>
         <p>Integramos un servicio de inteligencia artificial que analiza los datos recolectados y genera recomendaciones adecuadas para optimizar el rendimiento de los cultivos.
         </p>
