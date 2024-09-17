8ec7a36a8f34294d486a1523317c584bb9f6d30b
Pablo Gimenez <matiixd11@gmail.com>
2024-09-17 19:24:06 -0300

    Agregue una captura mas

--- src/components/TercerApartado/index.jsx
+++ src/components/TercerApartado/index.jsx
@@ -4,8 +4,11 @@ import styles from './TercerApartado.module.css'
 export default function TercerApartado() {
   return (
     <div id={styles.PrincipalContainer}>
-        <h1 id={styles.quienesSomos}>DESCARGA LA APP</h1>
-        <img src='./MenuPrincipalV1.3NoFondo.png' alt="" id={styles.capturaMain} />
+        <h1 id={styles.titulo}>DESCARGA LA APP</h1>
+        <div id={styles.capturas}>
+          <img src='./MenuPrincipalV1,4NoFondo.png' alt="" className={styles.captura} />
+          <img src='./Notificacion Riego.png' alt="" className={styles.captura} />
+        </div>
         <div id={styles.downloadSites}>
           <img src="./DownloadAppStore.png" alt="" id={styles.downloadAPP} />
           <img src="./DownloadPlayStore.png" alt="" id={styles.downloadAPP} />
