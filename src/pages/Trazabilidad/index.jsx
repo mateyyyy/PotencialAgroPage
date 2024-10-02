fb4aa0bce3db87f24daebbed601413bd06396fd7
Pablo Gimenez <matiixd11@gmail.com>
2024-10-02 19:33:02 -0300

    Cambios de trazabilidad

--- src/pages/Trazabilidad/index.jsx
+++ src/pages/Trazabilidad/index.jsx
@@ -5,12 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Trazabilidad() {
   return (<>
     <div id={styles.PrinDiv}>
-        <embed src="./Matriz Trazabilidad (1).pdf" type="" width="100%" height="100%"/>
-    </div>
-
-    <div id={styles.iconosSuperiores}>
-        <img src="./logo_potencial_agro.svg" alt="" id={styles.logo}/>
-        <Link to={"/"}><button id={styles.volverBoton}>Volver a la Potencial Agro</button></Link>
+      <iframe width="100%" height="100%" src="https://miro.com/app/live-embed/uXjVLdciF6o=/?moveToViewport=-1466,-125,4543,2155&embedId=573543244203" frameborder="0" scrolling="no" allow="fullscreen; clipboard-read; clipboard-write" allowfullscreen></iframe>
     </div>
 
   </>
