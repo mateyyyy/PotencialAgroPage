004546edbf3020c21873c8a7b6d44d25410aa86c
Pablo Gimenez <matiixd11@gmail.com>
2024-12-18 16:47:46 -0300

    Actualizacion de documentos

--- src/components/ProyectoApartado/index.jsx
+++ src/components/ProyectoApartado/index.jsx
@@ -9,6 +9,11 @@ import { PiListNumbersFill } from "react-icons/pi";
 import { FaBook } from "react-icons/fa";
 import { IoAlertCircleSharp } from "react-icons/io5";
 import { FaShoppingBag } from "react-icons/fa";
+import { TfiShoppingCartFull } from "react-icons/tfi";
+import { CiViewTimeline } from "react-icons/ci";
+import { FaPersonRays } from "react-icons/fa6";
+import { GrLink } from "react-icons/gr";
+import { GrDocumentConfig } from "react-icons/gr";
 
 
 export default function ProyectoApartado() {
@@ -23,54 +28,91 @@ export default function ProyectoApartado() {
             </div>
           </Link>
           
-          <Link to={"/alcance"} className={styles.link}>
+          <a href="https://potencialagro.atlassian.net/wiki/spaces/PA/pages/819264/Enunciado+del+Alcance+del+Proyecto" className={styles.link}>
             <div className={styles.cardInfo}>
               <GiArrowScope style={{ width: '5vw', height: 'auto' }}/>
               <h3 className={styles.cardInfoH3}>ALCANCE DEL PROYECTO</h3>
             </div>
-          </Link>
+          </a>
 
-          <Link to={"/acta"} className={styles.link}>
+          <a href="https://potencialagro.atlassian.net/wiki/spaces/PA/pages/885073/Acta+de+Constituci+n+del+Proyecto" className={styles.link}>
             <div className={styles.cardInfo}>
               <GrDocumentUser style={{ width: '5vw', height: 'auto' }}/>
               <h3 className={styles.cardInfoH3}>ACTA DE CONSTITUCION</h3>
             </div>
-          </Link>
+          </a>
 
-          <Link to={"/requisitos"} className={styles.link}>
+          <a href="https://potencialagro.atlassian.net/wiki/spaces/PA/pages/852285/Documento+de+Requisitos" className={styles.link}>
             <div className={styles.cardInfo}>
               <HiClipboardDocumentCheck  style={{ width: '5vw', height: 'auto' }}/>
               <h3 className={styles.cardInfoH3}>DOCUMENTO DE REQUISITOS</h3>
             </div>
-          </Link>
+          </a>
 
-          <Link to={"/trazabilidad"} className={styles.link}>
+          <a href="https://potencialagro.atlassian.net/wiki/spaces/PA/pages/819615/Matriz+de+trazabilidad" className={styles.link}>
             <div className={styles.cardInfo}>
               <PiListNumbersFill  style={{ width: '5vw', height: 'auto' }}/>
               <h3 className={styles.cardInfoH3}>MATRIZ DE TRAZABILIDAD</h3>
             </div>
-          </Link>
+          </a>
 
-          <Link to={"/diccionario"} className={styles.link}>
+          <a href="https://potencialagro.atlassian.net/wiki/spaces/PA/pages/2556106/Diccionario+EDT" className={styles.link}>
             <div className={styles.cardInfo}>
               <FaBook  style={{ width: '5vw', height: 'auto' }}/>
               <h3 className={styles.cardInfoH3}>DICCIONARIO EDT</h3>
             </div>
-          </Link>
+          </a>
 
-          <Link to={"/matrizRiesgo"} className={styles.link}>
+          <a href="https://potencialagro.atlassian.net/wiki/spaces/PA/pages/852482/Matriz+de+Riesgos" className={styles.link}>
             <div className={styles.cardInfo}>
               <IoAlertCircleSharp  style={{ width: '5vw', height: 'auto' }}/>
               <h3 className={styles.cardInfoH3}>MATRIZ DE RIESGOS</h3>
             </div>
-          </Link>
+          </a>
 
-          <Link to={"/adquisiciones"} className={styles.link}>
-            <div className={styles.cardInfo}>
+          <a href="https://potencialagro.atlassian.net/wiki/spaces/PA/pages/917674/Descripci+n+de+adquisiciones" className={styles.link}>
+          <div className={styles.cardInfo}>
               <FaShoppingBag  style={{ width: '5vw', height: 'auto' }}/>
               <h3 className={styles.cardInfoH3}>DOCUMENTO DE ADQUISICIONES</h3>
             </div>
-          </Link>
+          </a>
+
+        <a href="https://potencialagro.atlassian.net/wiki/spaces/PA/pages/98536/Gesti+n+de+los+costos+del+proyecto" className={styles.link}>
+        <div className={styles.cardInfo}>
+              <TfiShoppingCartFull  style={{ width: '5vw', height: 'auto' }}/>
+              <h3 className={styles.cardInfoH3}>GESTION DE COSTOS DEL PROYECTO</h3>
+            </div>
+        </a>
+
+        <a href="https://potencialagro.atlassian.net/wiki/spaces/PA/pages/918312/Gesti+n+del+cronograma" className={styles.link}>
+        <div className={styles.cardInfo}>
+              <CiViewTimeline   style={{ width: '5vw', height: 'auto' }}/>
+              <h3 className={styles.cardInfoH3}>GESTION DEL CRONOGRAMA</h3>
+            </div>
+        </a>
+
+        <a href="https://potencialagro.atlassian.net/wiki/spaces/PA/pages/819450/Perfil+del+l+der+del+proyecto" className={styles.link}>
+        <div className={styles.cardInfo}>
+              <FaPersonRays    style={{ width: '5vw', height: 'auto' }}/>
+              <h3 className={styles.cardInfoH3}>PERFIL DEL LIDER DEL PROYECTO</h3>
+            </div>
+        </a>
+
+        <a href="https://potencialagro.atlassian.net/wiki/spaces/PA/pages/852566/Actividades+de+Integraci+n" className={styles.link}>
+        <div className={styles.cardInfo}>
+              <GrLink    style={{ width: '5vw', height: 'auto' }}/>
+              <h3 className={styles.cardInfoH3}>ACTIVIDADES DE INTEGRACION</h3>
+            </div>
+        </a>
+
+        <a href="https://potencialagro.atlassian.net/wiki/spaces/PA/pages/2785290/Gesti+n+de+la+Configuraci+n" className={styles.link}>
+        <div className={styles.cardInfo}>
+              <GrDocumentConfig     style={{ width: '5vw', height: 'auto' }}/>
+              <h3 className={styles.cardInfoH3}>GESTION DE LA CONFIGURACION</h3>
+            </div>
+        </a>
+
+
 
       </div>
 
--- src/pages/EDT/index.jsx
+++ src/pages/EDT/index.jsx
@@ -4,6 +4,6 @@ import styles from './EDT.module.css'
 export default function index() {
   return (
     <div id={styles.EDTdiv}>
-<iframe width="100%" height="100%" src="https://miro.com/app/embed/uXjVLdPXU3A=/?pres=1&frameId=3458764600444426755&embedId=437506745855" frameborder="0" scrolling="no" allow="fullscreen; clipboard-read; clipboard-write" allowfullscreen></iframe>    </div>
+<iframe width="100%" height="100%" src="https://miro.com/app/live-embed/uXjVL59UJFc=/?moveToViewport=-2225,1205,5004,2374&embedId=176385584902" frameborder="0" scrolling="no" allow="fullscreen; clipboard-read; clipboard-write" allowfullscreen></iframe>    </div>
   )
 }
